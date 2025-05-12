9327677287251f23f0c9959dd64713d9efda2e42
vinayaka <vinayakah47@gmail.com>
2025-05-12 16:55:01 +0530

    Pen database

---
+++ Pen Database.sql
@@ -0,0 +1,9 @@
+create database pen;
+use pen;
+create table pen_info(id int,brand varchar(25),color varchar(25),price int(25));
+insert into pen_info values(1,"Renolds","Blue",5);
+insert into pen_info values(2,"Meow","Black",10);
+insert into pen_info values(3,"Natraja","Red",5);
+insert into pen_info values(4,"Bright Max","Blue",10);
+select*from pen_info;
+
