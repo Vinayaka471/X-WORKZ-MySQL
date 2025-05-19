cc098fc393c240521ef21705ec1303f005561270
vinayaka <vinayakah47@gmail.com>
2025-05-19 13:38:36 +0530

    Water Company Database

---
+++ Water Company Database.sql
@@ -0,0 +1,8 @@
+create database water;
+use water;
+create table water_info(id int,brand varchar(25),price int(25),color varchar(25));
+insert into water_info values(1,"Bindu",20,"Blue");
+insert into water_info values(2,"Bisleri",25,"Green");
+insert into water_info values(3,"Aqua",10,"Blue");
+insert into water_info values(4,"Jal",15,"Dark Green");
+select*from water_info;
