57b78a3f453fe6663b76a27696430d3ff2f68546
vinayaka <vinayakah47@gmail.com>
2025-05-24 09:48:41 +0530

    Hospital Management System

--- hospital management system.sql
+++ hospital management system.sql
@@ -29,3 +29,23 @@ insert into Room values("303","Availabil","3500","ICU Bed 4");
 insert into Room values("304","Availabil","3500","ICU Bed 5");
 insert into Room values("305","Availabil","3500","ICU Bed 6");
 
+create table department(Department varchar(100), Phone_no varchar(20));
+select*from department;
+
+insert into department values("Surgical department", "123456789");
+insert into department values("Nursing department", "123456789");
+insert into department values("Operation theator complex (OT)", "123456789");
+insert into department values("Paramedical department", "123456789");
+
+
+create table EMP_INFO(Name varchar(20), Age varchar(20), Phone_Number varchar(20), salary varchar(20),Gmail varchar(20), Aadhar_Number varchar(20));
+select*from EMP_INFO;
+
+insert into EMP_INFO values("Doctors1","30","123456789","50000","gr@gmail.com","123456789101");
+
+insert into EMP_INFO values("Doctors2","30","123456789","50000","gr@gmail.com","123456789101"); 
+
+
+
+
+
