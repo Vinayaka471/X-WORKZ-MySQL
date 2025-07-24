b137854fa164820e2adcca3d59958676bda84e06
vinayaka <vinayakah47@gmail.com>
2025-07-24 07:18:06 +0530

    Water Bottle Database

---
+++ Water Bottles Database.sql
@@ -0,0 +1,9 @@
+create database waterbottles;
+use waterbottles;
+create table waterbottles(id int,name varchar(25),price int,color varchar(25),location varchar(25));
+select*from waterbottles;
+insert into waterbottles values(1,'Bindu',25,'blue','karnataka');
+insert into waterbottles values(2,'Aqua',35,'white','mumbai');
+insert into waterbottles values(3,'Kinley',15,'green','kolkata');
+insert into waterbottles values(4,'Rail water',30,'maroon','jammu&kashmir');
+select*from waterbottles;
