3efcf93727bd238884b3408e7bd806c820d27c81
vinayaka <vinayakah47@gmail.com>
2025-09-13 17:54:19 +0530

    Spring database

--- pgdb database.sql
+++ pgdb database.sql
@@ -43,12 +43,46 @@ insert into user_roles_mapping(user_role_id, user_id, role_id) values(201, 101, 
 insert into subject (subject_id, subject_name, subject_code) values(301, 'Mathematics', 'MATH101'), (302, 'Computer Science', 'CS101');
 
 insert into user_subject_mapping (user_subject_id, user_id, subject_id) values (401, 101, 301), (402, 102, 302);
+
+
+create table mcq (question_id int primary key, question varchar(500) not null, option_1 varchar(255) not null, option_2 varchar(255) not null, option_3 varchar(255) not null, option_4 varchar(255) not null, correct_answer varchar(255) not null);
+
+create table mcq_test (test_id int primary key, test_name varchar(100) not null, test_link varchar(255) unique not null);
+
+create table test_question_mapping (test_question_mapping_id int primary key, test_id int not null, question_id int not null, foreign key (test_id) references mcq_test(test_id) on delete cascade, foreign key (question_id) references mcq(question_id) on delete cascade);
  
+ 
+ create table test_response (test_response_id int primary key, test_id int not null, question_id int not null, user_id int not null, response varchar(255), foreign key (test_id) references mcq_test(test_id) on delete cascade, foreign key (question_id) references mcq(question_id) on delete cascade, foreign key (user_id) references users(user_id) on delete cascade);
+
+
+insert into mcq (question_id, question, option_1, option_2, option_3, option_4, correct_answer) values (501, 'What is 2 + 2?', '3', '4', '5', '6', '4'), (502, 'Which language is used for web styling?', 'HTML', 'CSS', 'Java', 'Python', 'CSS'), (503, 'Who developed Java?', 'Microsoft', 'Sun Microsystems', 'Apple', 'Google', 'Sun Microsystems');
+
+insert into mcq_test (test_id, test_name, test_link) values  (601, 'Math Basic Test', 'http://example.com/test/math1'), (602, 'CS Fundamentals Test', 'http://example.com/test/cs1');
+
+insert into test_question_mapping (test_question_mapping_id, test_id, question_id) values
+(701, 601, 501), -- Math test -> 2+2
+(702, 602, 502), -- CS test -> CSS
+(703, 602, 503); -- CS test -> Java
+
+insert into test_response (test_response_id, test_id, question_id, user_id, response)
+values
+(801, 601, 501, 102, '4'),           -- Student answered Math correctly
+(802, 602, 502, 102, 'HTML'),        -- Student answered CS wrong
+(803, 602, 503, 102, 'Sun Microsystems'); -- Student answered CS correct
+
+
+
 select * from users;
 select * from roles;
 select * from user_roles_mapping;
 select * from subject;
 select * from user_subject_mapping;
+select * from mcq;
+select * from mcq_test;
+select * from test_question_mapping;
+select * from test_response;
+
+
 
 
 
